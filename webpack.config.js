04ce5c4fe57e96a8e6329eaa12696aa58882a714
Egor Gugaev <egor@gugaevegor.ru>
2018-10-24 19:57:30 +0300

    Минифицированные изображения

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-var environment = process.env.NODE_ENV || 'development';
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const environment = process.env.NODE_ENV || 'development';
 
 module.exports = {
     entry: {
@@ -44,13 +45,19 @@ module.exports = {
         new CopyWebpackPlugin([
             {from:'./src/assets',to:'img'} 
         ]),
+        new ImageminPlugin({
+            pngquant: {
+              quality: '95-100'
+            }
+          }),
         new webpack.DefinePlugin({'process.env.NODE_ENV': '"' + environment + '"'}),
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
             }
-        })
+        }),
+        
     ],
 
     devServer: {
